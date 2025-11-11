5c4ff738770fb2fb9569c633bf129e318bf5385e
Gustaf Sjoberg <gs@trell.se>
2025-11-11 19:56:58 +0100

    Add migration runners.

--- src/he_kit/utils/alembic.py
+++ src/he_kit/utils/alembic.py
@@ -1,4 +1,6 @@
 from alembic.config import Config
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from .project import find_project_root, get_settings
 
@@ -25,3 +27,34 @@ def get_alembic_config() -> Config:
     cfg.config_file_name = str(ini_path)
 
     return cfg
+
+
+def run_migrations_offline(context, target_metadata):
+    # turn url into sync if it is async?
+    config = context.config
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def async_run_migrations_online(context, target_metadata):
+    # turn url into async if it is sync?
+    config = context.config
+    connectable = create_async_engine(
+        config.get_main_option("sqlalchemy.url"),
+        poolclass=pool.NullPool,
+    )
+    async with connectable.connect() as connection:
+        await connection.run_sync(
+            lambda conn: context.configure(
+                connection=conn,
+                target_metadata=target_metadata,
+            )
+        )
+
+        def do_run_migrations(sync_connection):
+            with sync_connection.begin():
+                context.run_migrations()
+
+        await connection.run_sync(do_run_migrations)
